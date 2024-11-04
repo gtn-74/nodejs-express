6e376a3740cb5d23900c4670879ba3b486b51436
gtn-74 <gtn.r74@gmail.com>
2024-11-04 20:45:06 +0900

    add memo

--- express/2/main.js
+++ express/2/main.js
@@ -5,14 +5,16 @@ const list = document.querySelector(".tasks");
 
 let todos = [];
 
+// post
 function addTodo(text) {
   const todo = {
-    id: Date.now(),
-    text,
+    id: Date.now(), // 入力時間をidとしてる
+    text, // 入力値を文字列としている
   };
   todos.push(todo);
 }
 
+// get
 function renderTodo() {
   let temp = "";
   todos.forEach((todo) => {
@@ -23,13 +25,14 @@ function renderTodo() {
         `;
     temp += html;
   });
-
   list.innerHTML = temp;
 }
 
+// 送信Enter押下後に走るイベント
 addTask.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault(); // !()がないイベント発生しない
 
+  // ? 入力値の取得？
   const text = addTask.add.value.trim();
   if (text !== "") {
     addTodo(text);
