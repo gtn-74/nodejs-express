0242efc0f6fc052b57076d6e504964c0879ed2f1
gtn-74 <gtn.r74@gmail.com>
2024-11-05 23:14:23 +0900

    Chapter 10:Node.jsでHTTPサーバを構築しよう

---
+++ express/3/js_study/http_server/main.js
@@ -0,0 +1,50 @@
+// プロトコル通信を指定
+const http = require("http") // !Node.jsがPC内に存在していれば、利用できるモジュール
+const port = 3000
+
+// 
+const server = http.createServer((request,response)=>{
+// * - 第一引数：ステータス行：HTTPリクエストが成功したか失敗したかを表すデータが入る
+// * - 第二引数：ヘッダー：送信するデータの情報など、追加情報が入る
+response.writeHead(/* ステータスコード */ 200,{
+ /* メディアタイプを指定 */ "Content-Type": "text/html"
+})
+// * - 本文：データの内容
+const responseMessage = "<h1>Hello World</h1>"
+response.end(responseMessage)
+console.log(`Sent a response: ${responseMessage}`);
+})
+// httpレスポンス
+/**
+ * - ステータス行：HTTPリクエストが成功したか失敗したかを表すデータが入る
+ * @name response.writeHead()
+ * - ヘッダー：送信するデータの情報など、追加情報が入る
+ * @http status code
+ * - 本文：データの内容が入る
+ * */
+
+/**
+ * text/plain：プレーンテキスト
+ * text/html：HTMLドキュメント
+ * application/json：JSONデータ
+ * image/png:PNG画像
+ * 
+ * */ 
+
+// ポート
+// ネットワークがサーバーを通るドア的なもの 0~65,535番まで存在する
+// ポートが空いていないと通信ができない
+// ポートが待機状態じゃない場合も通信できない(listen())
+
+server.listen(port)
+console.log(`The server has started and is listening on port number: ${port}`);
+
+// `http://localhost:3000/`
+
+// localhost
+// サーバーの住所。IPアドレス、ドメイン名を指す
+// localhostは特殊で、自分のPCを指す
+
+// ドメイン名 = DSNで変換したIPアドレス
+
+// localhost = `127.0.0.1`
