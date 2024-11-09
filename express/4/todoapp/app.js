ffdc6efcfff1c9fe34be703688efa197395eb1fc
gtn-74 <gtn.r74@gmail.com>
2024-11-09 14:43:33 +0900

    アプリ立ち上げ　画面表示

--- express/4/todoapp/app.js
+++ express/4/todoapp/app.js
@@ -5,12 +5,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const usersRouter = require('./routes/users');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+// Reactを利用する場合、`jsxt`となる
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -19,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// router
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- express/4/todoapp/routes/index.js
+++ express/4/todoapp/routes/index.js
@@ -4,6 +4,11 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  // res.render(view, [locals], [callback])
+  // view:レンダリングするビューの名前。上だとindexファイル
+  // locals:ビューに渡すローカル変数を含むオブジェクト。【ぶっちゃけよくわからん】
+  // callback:レンダリング完了後に呼び出される関数
 });
+
 
 module.exports = router;
